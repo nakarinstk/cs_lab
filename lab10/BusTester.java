ae0c944088b98546b829f86e091b0db90d68d478
charleshx <charleshxi@gmail.com>
2019-04-01 00:54:46 +0700

    fix order lab 10.2

--- lab10/BusTester.java
+++ lab10/BusTester.java
@@ -6,8 +6,8 @@ public class BusTester {
 
         ArrayList<Bus> arr = new ArrayList<Bus>();
 
-        arr.add(new CNGBus(50, 1000000, 200, 2));
         arr.add(new Hybrid(45, 1200000, 600, 150, 1));
+        arr.add(new CNGBus(50, 1000000, 200, 2));
         for (int i = 0; i < arr.size(); i++) {
             System.out.println("ID: " + arr.get(i).getID());
             LiquidFuel foo = (LiquidFuel) arr.get(i);
