1618456108bff302b1398bf5a0b7eed39bf9dd44
charleshx <charleshxi@gmail.com>
2019-04-01 00:51:44 +0700

    fix bud 10.2

--- lab10/BusTester.java
+++ lab10/BusTester.java
@@ -9,8 +9,8 @@ public class BusTester {
         arr.add(new CNGBus(50, 1000000, 200, 2));
         arr.add(new Hybrid(45, 1200000, 600, 150, 1));
         for (int i = 0; i < arr.size(); i++) {
+            System.out.println("ID: " + arr.get(i).getID());
             LiquidFuel foo = (LiquidFuel) arr.get(i);
-            System.out.println("ID: " + (i + 1));
             System.out.println("Emmision Tier: " + foo.getEmmisionTier());
             System.out.println("Accel: " + arr.get(i).getAccel());
         }
