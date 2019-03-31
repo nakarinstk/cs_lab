7699951787ca2bc746c2f37a3477f12cc7f0f85f
charleshx <charleshxi@gmail.com>
2019-04-01 00:58:51 +0700

    fix order fix bug

--- lab10/BusTester.java
+++ lab10/BusTester.java
@@ -8,11 +8,10 @@ public class BusTester {
 
         arr.add(new Hybrid(45, 1200000, 600, 150, 1));
         arr.add(new CNGBus(50, 1000000, 200, 2));
-        for (int i = 0; i < arr.size(); i++) {
-            System.out.println("ID: " + arr.get(i).getID());
-            LiquidFuel foo = (LiquidFuel) arr.get(i);
-            System.out.println("Emmision Tier: " + foo.getEmmisionTier());
-            System.out.println("Accel: " + arr.get(i).getAccel());
+        for (Bus car : arr) {
+            System.out.println("ID: " + car.getID());
+            System.out.println("Emmision Tier: " + ((LiquidFuel) car).getEmmisionTier());
+            System.out.println("Accel: " + car.getAccel());
         }
 
     }
